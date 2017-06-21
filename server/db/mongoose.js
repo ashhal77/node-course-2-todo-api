f02dfdf4e34ca5e580eb671721b6dccc8e70c83b
admin_tunnus <admin_tunnus>
2017-06-21 21:26:38 +0300

    umongoose file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds135252.mlab.com:35252/nodejs');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
 
 
 module.exports = {mongoose: mongoose};
