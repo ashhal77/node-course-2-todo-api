037b118582af58a3b5b70061a36c67400d438382
admin_tunnus <admin_tunnus>
2017-06-22 03:58:38 +0300

    test the delete/todos id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -109,3 +109,33 @@ it('should return 404 for non object ids', (done)=>{
 	});
 
 });
+
+
+describe('Delete/todos/:id', ()=>{
+	it('should remove a todo', (done)=>{
+    var hexId = todos[1]._id.toHexString();
+    request(app).delete(`/todos/${hexId}`).expect(200)
+    .expect((res)=>{
+    	expect(res.body.todo._id).toBe(hexId);
+    })
+    .end((err, res)=>{
+    	if(err){
+    		return done (err);
+    	}
+
+    	Todo.findById(hexId).then((todo)=>{
+    		expect(todo).toNotExist();
+    		done();
+    	}).catch((e)=> done(e));
+    });
+	});
+
+	/*it('should return 404 if todo not found', (done)=>{
+
+	});
+
+
+	it('should reutrn 404 if objectID is invalid', (done)=>{
+
+	});*/
+});
